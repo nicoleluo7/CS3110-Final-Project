03745c5687587305f0b8c6145b8c28e55b0fbbff
Shirleywu537 <ww537@cornell.edu>
2025-11-17 14:48:26 +0000

    Part B: Implement sequent (proof state representation) data structure with helper functions and apply Modus Ponens rule, also add comments

---
+++ lib/sequent.ml
@@ -0,0 +1,79 @@
+open Ast
+open Rule
+
+type t = {
+  premises : prop list;
+  derived : prop list;
+  goal : prop option;
+}
+(** type t represents a record of premises, what's derived and the goal.
+    premises is a list of prop, derived is a list of prop and goal is an option
+    that is none by default and can be set to some p. *)
+
+(** empty initiates an empty t with premises and derived empty and goal set to
+    none. *)
+let empty = { premises = []; derived = []; goal = None }
+
+(** add_premise adds a prop to the original list. *)
+let add_premise t p = { t with premises = p :: t.premises }
+
+(** add_derived adds a prop to the original list. *)
+let add_derived t p =
+  if List.mem p t.derived || List.mem p t.premises then t
+  else { t with derived = p :: t.derived }
+
+(** add_goal takes in two arguments, first is a record of type t and second is a
+    valid prop p, then it sets the goal of t to Some p. *)
+let add_goal t p = { t with goal = Some p }
+
+(** apply_modus_ponens applies modus ponens rule to all props in the premises
+    and derived list of the record and adds any potential new props to the
+    derived list. *)
+let rec apply_modus_ponens st =
+  let known = st.premises @ st.derived in
+
+  (* Compute all new propositions that can be inferred in this round *)
+  let new_props =
+    List.fold_left
+      (fun acc p1 ->
+        List.fold_left
+          (fun acc2 p2 ->
+            match modus_ponens p1 p2 with
+            | Some p
+              when (not (List.mem p st.premises))
+                   && (not (List.mem p st.derived))
+                   && not (List.mem p acc) -> p :: acc2
+            | _ -> acc2)
+          acc known)
+      [] known
+  in
+  match new_props with
+  | [] -> st
+  | _ ->
+      let new_st = List.fold_left add_derived st new_props in
+      apply_modus_ponens new_st
+
+(** judge_goal returns the result of the goal. If the goal is None or doesn't
+    exist in premises and derived lists, then it will return false, otherwise,
+    it will return true. *)
+let judge_goal t =
+  match t.goal with
+  | None -> false
+  | Some p ->
+      List.exists (( = ) p) t.premises || List.exists (( = ) p) t.derived
+
+(** print_result prints the contents of the record of type t in a concise way.
+*)
+let print_result t =
+  print_endline "Proof State:";
+  print_endline "Premises:";
+  List.iter (fun p -> print_endline (" - " ^ prop_to_string p)) t.premises;
+  print_endline "Derived:";
+  List.iter (fun p -> print_endline (" - " ^ prop_to_string p)) t.derived;
+  print_endline
+    ("Goal: "
+    ^
+    match t.goal with
+    | None -> "None"
+    | Some p -> prop_to_string p);
+  print_endline (if judge_goal t then "Goal reached!" else "Goal not reached")
